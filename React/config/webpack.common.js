0877f10263df03174531e5ec99962511b153ccea
HAVET <vhavet@gmail.com>
2020-05-21 16:39:04 +0200

    fix react modele

--- React/config/webpack.common.js
+++ React/config/webpack.common.js
@@ -28,7 +28,7 @@ module.exports = {
     ]),
 
     new HtmlWebpackPlugin({
-      favicon: paths.assets + '/favicon.ico',
+      favicon: paths.assets + '/logo.png',
       template: paths.assets + '/index.html',
     }),
   ],
